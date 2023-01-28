252f1584ea38489081202ca311c2ce73a6c0d9e2
WATANABE Hirotada <beqpad@hotmail.com>
2023-01-28 16:11:24 +0900

    コードの作成準備

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+
+if (args.Length < 1)
+{
+    WriteUsage();    
+    return;
+}
+
+var path = args[0];
+
+if (!File.Exists(path))
+{
+    Console.WriteLine($"Specified parameter is not a path for private key pem file.");
+    WriteUsage();
+    return;
+}
+
+
+var pem = await File.ReadAllTextAsync(path);
+var rsa = RSA.Create();
+rsa.ImportFromPem(pem);
+
+
+// TODO 
+
+
+
+void WriteUsage()
+{
+    Console.WriteLine("""
+        Usage> TestGraphQL <pem file path>
+        """);
+
+}
