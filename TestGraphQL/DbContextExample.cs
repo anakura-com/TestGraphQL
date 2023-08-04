0141489e0c74485b3f106ae51bc47a89c858a698
Watanabe Hirotada <hiro@anakura.com>
2023-08-04 10:07:37 +0900

    New branch01 (#6)
    * ダメなコード
    
    * ダメなコード　テストで落ちる
    
    * 脆弱性があることをテストする
    
    * .NET 7 へ
    
    * ある程度修正
    
    * テストコードを修正
    
    * カバレッジ追加
    
    * add dotnet-coverage
    
    * dotnet-coverage のインストールとパス
    
    * specify coverage report parameter
    
    * install dotnet-coverage as global tools
    
    * update coverage report
    
    * テストコードを修正
    
    * Update Program.cs
    
    * with smels
    
    * workflow
    
    * exclude DbContext

---
+++ TestGraphQL/DbContextExample.cs
@@ -0,0 +1,13 @@
+// namespace TestGraphQL;
+
+// using Microsoft.EntityFrameworkCore;
+// using System.Data.Entity;
+
+// public class DbContextExample : DbContext
+// {
+//     public DbSet<Example> Examples { get; set; }
+
+//     public DbContextExample(DbContextOptions<DbContextExample> options) : base(options)
+//     {
+//     }
+// }
